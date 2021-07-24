5dbf8458856c64e1b1c3517123663fb239d95039
kwynncom <buessresume@gmail.com>
2021-07-24 18:05:08 -0400

    perhaps final get csv in function form

--- csv.php
+++ csv.php
@@ -2,19 +2,32 @@
 
 require_once('PRIVATE_config.php');
 
-$t = file_get_contents(KWYNN_INIT_CSV_PATH);
-$l = strlen($t);
+$biga = getCSVA();
 
-$a10 = explode("\n", $t);
 
-$biga = [];
-$i = 0;
-foreach($a10 as $r) {
-	$a = str_getcsv($r);
-	if (!isset($a[0]) || !$a[0]) continue;
-	$biga[$i] = $a;
-	$i++;
-	continue;
+function getCSVA() {
+
+	$t = file_get_contents(KWYNN_INIT_CSV_PATH);
+	$l = strlen($t);
+
+	$a10 = explode("\n", $t);
+
+	$biga = [];
+	$i = 0;
+	foreach($a10 as $r) {
+		$a = str_getcsv($r);
+		if (!isset($a[0])) continue;
+		$u = $a[0];
+		if (!$u || !is_string($u));
+		if (strpos($u, 'http') === false) continue;
+		$biga[$i] = $a;
+		$i++;
+		continue;
+	}
+
+	$cnt = count($biga);
+	return $biga;
+
 }
 
 exit(0);
