c8c0e9955370a25569c5afdfcf0d5249de9e0408
kwynncom <buessresume@gmail.com>
2021-07-24 20:05:23 -0400

    much more correct user usage

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ require_once('/opt/kwynn/kwutils.php');
 
 function getUserPath() {
 	if (isKwDev()) return __DIR__ . '/../users/';
-	// if (isAWS  ()) return __DIR__ . '/../../';
+	if (isAWS  ()) return __DIR__ . '/../../../20/07/users/';
 	
 }
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
-require_once('csv.php');
 require_once('config.php');
+require_once('users.php');
+
+validUserOrDie();
+
+require_once('csv.php');
 
 $G_BIGLA_JSON = json_encode(getCSVA());
 
@@ -10,6 +14,4 @@ $G_UPATH = getUserPath();
 require_once($G_UPATH  . 'users.php');
 require_once('template.php');
 
-require_once('users.php');
 
-validUserOrDie();
